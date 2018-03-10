3ffe941505dedd250fe80365815d25faee5ecef3
Soufiane Salouf <soufiane.salouf@gmail.com>
2018-03-10 16:05:10 +0000

    end lesson 5
    Detecting taps on 3D objects

--- RampUp-app/RampUp-app/Controller/RampPlacerVC.swift
+++ RampUp-app/RampUp-app/Controller/RampPlacerVC.swift
@@ -84,11 +84,16 @@ class RampPlacerVC: UIViewController, ARSCNViewDelegate , UIPopoverPresentationC
     
     @IBAction func onRampBtnPressed(_ sender: UIButton) {
         let rampPickerVC = RampPickerVC(size: CGSize(width: 250, height: 500))
+        rampPickerVC.rampPlacerVC = self
         rampPickerVC.modalPresentationStyle = .popover
         rampPickerVC.popoverPresentationController?.delegate = self
         present(rampPickerVC, animated: true, completion: nil)
         rampPickerVC.popoverPresentationController?.sourceView = sender
         rampPickerVC.popoverPresentationController?.sourceRect = sender.bounds
+        
+    }
+    
+    func onRampSelected(_ rampName: String) {
         
     }
     
